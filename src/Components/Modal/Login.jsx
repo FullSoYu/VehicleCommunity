2ebd93aaffa5c75f0439f35d9e5c356c0975b36a
FullSoYu <hey6410@naver.com>
2022-12-15 18:13:58 +0900

    로그인 모달창 프론트 폼 완성 기능 구현 모달창 닫기 외부클릭 구현중

--- src/Components/Modal/Login.jsx
+++ src/Components/Modal/Login.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "../../Style/Modal/Login.scss";
-
 function Login({ setModalOpen }) {
   const closeModal = () => {
     setModalOpen(false);
@@ -8,14 +7,6 @@ function Login({ setModalOpen }) {
 
   return (
     <div className="Login_Modal_Box">
-      {/* 모달창의 백그라운드 이미지 */}
-      {/* <div className="Background_img_box">
-        <img
-          src="https://files.porsche.com/filestore/news/international-de/none/582761/headimage4/cc7e9593-5d35-11ea-80c8-005056bbdc38/Definitely-911%2c-definitely-Turbo%2c-definitely-new-Porsche-911-Turbo-S.jpg"
-          alt=""
-        />
-      </div> */}
-      {/* ID & Password 입력 창 */}
       <div className="Login_Input_Box">
         <div className="Login_Input_Box_Title">
           <img src="https://ifh.cc/g/74JzFs.png" onClick={closeModal} alt="" />
@@ -52,12 +43,23 @@ function Login({ setModalOpen }) {
       <div className="Login_Sns_Title_Area">
         <strong>간편하게 시작하기</strong>
         <br />
-        <div>네이버</div>
-        <div>카카오</div>
-        <div>구글</div>
-        <div>애플</div>
+        <div className="Sns_Icons_Box">
+          <button className="Sns_Button_Common_Style">
+            <div className="Sns_Icons_Naver Sns_Icons_Common_Style"></div>네이버
+          </button>
+          <button className="Sns_Button_Common_Style">
+            <div className="Sns_Icons_Kakao Sns_Icons_Common_Style"></div>카카오
+          </button>
+          <button className="Sns_Button_Common_Style">
+            <div className="Sns_Icons_Google Sns_Icons_Common_Style"></div>구글
+          </button>
+          <button className="Sns_Button_Common_Style">
+            <div className="Sns_Icons_Apple Sns_Icons_Common_Style"></div>애플
+          </button>
+        </div>
         <p>공용 PC에서는 소셜로그인 시 계정 관리에 유의해주세요.</p>
       </div>
+      <p className="Login_Box_Footer">ⓒ Vehicle Community</p>
     </div>
   );
 }
